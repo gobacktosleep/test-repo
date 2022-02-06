1f3a79693d3bb171cda475f348f27cec0d91bfeb
Gulnaz <egredimini@gmail.com>
2022-02-05 22:11:30 -0800

    feat: add music load function

---
+++ audio-player/js/index.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector('.wrapper'),
+fullScreenCover = document.querySelector('.fullscreen-cover'),
+coverImg = wrapper.querySelector('.img-area img'),
+audioName = wrapper.querySelector('.song-details .songname'),
+audioArtist = wrapper.querySelector('.song-details .artist'),
+mainAudio = wrapper.querySelector('.main-audio');
+
+
+let audioIndex = 1;
+
+// Вызов функции загрузки музыки после загрузки страницы
+
+window.addEventListener('load', () => {
+    loadAudio(audioIndex);
+});
+
+// Функция загрузки музыки
+
+function loadAudio(indexNumb) {
+    audioName.innerText = playlist[indexNumb - 1].songname;
+    audioArtist.innerText = playlist[indexNumb - 1].artist;
+    fullScreenCover.src = `assets/img/${playlist[indexNumb - 1].img}.jpg`;
+    coverImg.src = `assets/img/${playlist[indexNumb - 1].img}.jpg`;
+    mainAudio.src = `assets/music/${playlist[indexNumb - 1].src}.mp3`;
+};
+
