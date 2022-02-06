38e5c76b11b8f3296e71b632fa31810f970454e7
Gulnaz <egredimini@gmail.com>
2022-02-05 16:40:37 -0800

    feat: create an array to store music details

---
+++ audio-player/js/data.js
@@ -0,0 +1,22 @@
+let playlist = [
+    {
+        songname: 'Solstice',
+        artist: 'Laki Mera',
+        img: '1',
+        src: 'solstice'
+    },
+
+    {
+        songname: 'The Pot',
+        artist: 'Tool',
+        img: '2',
+        src: 'the_pot'
+    },
+
+    {
+        songname: 'Sickness Unto Foolish Death',
+        artist: 'Akira Yamaoka',
+        img: '3',
+        src: 'sickness_unto_foolish_death'
+    }
+]
