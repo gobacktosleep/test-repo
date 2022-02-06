abb4a3aadd6d58cf878e0f87bb8d096814982f13
Gulnaz <egredimini@gmail.com>
2022-02-05 22:40:23 -0800

    feat: add play/pause button

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -3,7 +3,9 @@ fullScreenCover = document.querySelector('.fullscreen-cover'),
 coverImg = wrapper.querySelector('.img-area img'),
 audioName = wrapper.querySelector('.song-details .songname'),
 audioArtist = wrapper.querySelector('.song-details .artist'),
-mainAudio = wrapper.querySelector('.main-audio');
+mainAudio = wrapper.querySelector('.main-audio'),
+playPauseButton = wrapper.querySelector('.play-pause');
+
 
 
 let audioIndex = 1;
@@ -24,3 +26,21 @@ function loadAudio(indexNumb) {
     mainAudio.src = `assets/music/${playlist[indexNumb - 1].src}.mp3`;
 };
 
+// Проигрывание и остановка звука нажатием на кнопку
+
+function playAudio() {
+    wrapper.classList.add('pause');
+    playPauseButton.src = './assets/svg/pause.svg';
+    mainAudio.play();
+};
+
+function pauseAudio() {
+    wrapper.classList.remove('pause');
+    playPauseButton.src = './assets/svg/play.svg';
+    mainAudio.pause();
+};
+
+playPauseButton.addEventListener('click', () => {
+    const isPaused = wrapper.classList.contains('pause');
+    isPaused ? pauseAudio() : playAudio();
+});
